5dfb2f54bc55f586ded1dfc6ae95d56c9f733a6f
GOURILEKSHMI.S <gourilekshmi.s@btech.christuniversity.in>
2026-08-27 12:15:57 +0530

    Enhance task1.js with comments and output message
    Added comments to explain the purpose of the script.

--- task1.js
+++ task1.js
@@ -1,1 +1,9 @@
-console.log('Countdown App Ready');
+// Task 1: Initialize and run a Node.js application
+
+// This simple program checks whether Node.js is working
+// by printing a message on the terminal.
+
+console.log("Countdown App Ready!");
+
+// Output:
+// Countdown App Ready!
